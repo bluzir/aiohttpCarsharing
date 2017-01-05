5a9a13c8ce669fa77edfdf284025df220bd74838
Vladislav <kouklevv@gmail.com>
2017-01-05 10:30:37 +0300

    Add main handler, templates and static files

--- main.py
+++ main.py
@@ -1,29 +1,39 @@
-import logging
+import os
+
+import tornado.escape
+import tornado.ioloop
+import tornado.options
+import tornado.web
+import tornado.websocket
 
 
 # websocket example
-import tornado as tornado
-
-
 class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
             (r"/", MainHandler),
             (r"/websocket", WebSocketHandler),
         ]
+        settings = dict(
+            template_path=os.path.join(os.path.dirname(__file__), "templates"),
+            static_path=os.path.join(os.path.dirname(__file__), "static"),
+            xsrf_cookies=True,
+        )
+        super(Application, self).__init__(handlers, **settings)
 
 
-class MainHandler:
-    pass
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("index.html")
 
 
-class WebSocketHandler:
+class WebSocketHandler(tornado.web.RequestHandler):
     pass
 
 
 def main():
     app = Application()
-    app.listen(8000)
+    app.listen(8080)
     tornado.ioloop.IOLoop.current().start()
 
 
