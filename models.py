6cc7cbe8505041ad623f2e18421144ff7a44669f
Vladislav <kouklevv@gmail.com>
2017-01-09 12:23:39 +0300

    Add peewee async example

--- models.py
+++ models.py
@@ -14,6 +14,29 @@ class Users(peewee.Model):
         max_length=30,
     )
 
+    class Meta:
+        database=database
+
 Users.create(login='test', password='test')
+
 database.close()
+
+objects = peewee_async.Manager(database)
+database.set_allow_sync(False)
+
+async def handler():
+    await objects.create(Users, text="Not bad. Watch this, I'm async!")
+    all_objects = await objects.execute(Users.select())
+    for obj in all_objects:
+        print(obj.login, obj.password)
+
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(handler())
+loop.close()
+
+
+
+
 
