d1eed6fa987641cda6f78692861103b6b0c897a6
Vladislav <kouklevv@gmail.com>
2017-01-26 14:16:41 +0300

    Inplat api usage example

--- inplat_wrapper/api.py
+++ inplat_wrapper/api.py
@@ -1,13 +1,20 @@
 import hashlib
 import hmac
+import json
 
 import requests
 
 
+class InplatException:
+    def __init__(self):
+        self.error = None
+        self.text = None
+
+
 class InplatClient:
     API_KEY = 'F5aPOR2Zm3vHFQXVBjLgpnub'
-    DEFAULT_HOST = 'https://demo-api2.inplat.ru/{}'
-    SECRET_WORD = 'B1BUnfwEE2mAUK4D'
+    DEFAULT_HOST = 'https://demo-api2.inplat.ru/'
+    SECRET_WORD = b'B1BUnfwEE2mAUK4D'
 
     def __init__(self):
         self.sign = None
@@ -15,7 +22,6 @@ class InplatClient:
 
         self.params = {
             'api_key': self.API_KEY,
-            'sign': self.sign,
         }
 
         self.pay_type = None
@@ -91,21 +97,46 @@ class InplatClient:
     # Basic request with self.data
     def request(self):
         if self.generate_sign():
-            return requests.post(url=self.DEFAULT_HOST,
-                                 params=self.params,
-                                 data=self.data)
+            self.params.update({'sign': self.sign})
+            response = requests.post(url=self.DEFAULT_HOST,
+                                     params=self.params,
+                                     data=self.data)
+            if response.status_code == 200:
+                return response
+            else:
+                print(response.status_code)
+                print(response.text)
+                return False
         else:
             print('Problem with generating sign')
             return False
 
     def generate_sign(self):
         try:
-            self.sign = hmac.new(self.SECRET_WORD,
-                                 msg=self.data,
+            json_data = json.dumps(self.data).encode('utf-8')
+            generated = hmac.new(self.SECRET_WORD,
+                                 msg=json_data,
                                  digestmod=hashlib.sha256).hexdigest()
+            print(generated)
+            self.sign = generated
+            return True
         except Exception as e:
             print(e)
             return False
 
 
-
+if __name__ == '__main__':
+    client = InplatClient()
+    init = client.init(
+        pay_type='mc',
+        client_id='hp1L18kmOWVegdka30',
+        pay_params={"msisdn":79265327068},
+        params={
+            "account": "test",
+            "sum": 1023,
+            "email": "pay@example.com",
+            "details": "Оплата благотворительности",
+            "address": "ул. Пролетарская 23",
+        }
+    )
+    print(init.json())
