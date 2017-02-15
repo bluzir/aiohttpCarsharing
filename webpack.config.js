d9260d3d4bbeb903add56296ee191c59ef71a6cd
Vladislav <kouklevv@gmail.com>
2017-02-15 12:03:11 +0300

    Webpack config Bundle all JS files into bundle.js

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: ['whatwg-fetch', './static/js/profile.js', './static/js/tariff.js', './static/js/payments.js'],
+  output: { path: __dirname, filename: 'bundle.js' },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+};
