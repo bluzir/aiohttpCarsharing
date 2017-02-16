6265f59af19a097c6cd1288b6d184ca1536c766b
Vladislav <kouklevv@gmail.com>
2017-02-16 23:11:00 +0300

    Server and settings

--- base_settings.py
+++ base_settings.py
@@ -1,3 +1,5 @@
+import os
+
 from config.stage_settings import *
 
 # Folders settings
--- server.py
+++ server.py
@@ -1,19 +1,27 @@
 import asyncio
-import logging
 
 import aiohttp_jinja2
 import jinja2
 from aiohttp import web
-from aiohttp_session import setup
+from aiohttp_session import setup, get_session, session_middleware
 from aiohttp_session.cookie_storage import EncryptedCookieStorage
 
 import base_settings as config
 from routes import setup_routes
 
-logging.basicConfig(level=config.LEVEL)
+import logging
+
+logging.basicConfig(filename='log/main.log', level=logging.DEBUG)
 
 
-def main():
+app = web.Application()
+setup_routes(app)
+setup(app, EncryptedCookieStorage(config.SECRET_KEY))
+aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader(config.TEMPLATES_ROOT))
+app.router.add_static('/static', config.STATIC_ROOT, show_index=True)
+
+
+if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     app = web.Application(loop=loop)
     setup_routes(app)
@@ -21,10 +29,4 @@ def main():
     aiohttp_jinja2.setup(app,
                          loader=jinja2.FileSystemLoader(config.TEMPLATES_ROOT))
     app.router.add_static('/static', config.STATIC_ROOT, show_index=True)
-
     web.run_app(app, host=config.HOST, port=config.PORT)
-
-if __name__ == '__main__':
-    main()
-
-
