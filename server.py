82643cf4bfe495bfaac4558c5b11c399443ef95e
Andrei Bodrov <ap.bodrov@gmail.com>
2017-02-16 18:23:17 +0300

    fix server.py

--- server.py
+++ server.py
@@ -1,7 +1,3 @@
-import asyncio
-import logging
-import ssl
-
 import aiohttp_jinja2
 import jinja2
 from aiohttp import web
@@ -11,21 +7,9 @@ from aiohttp_session.cookie_storage import EncryptedCookieStorage
 from config import base_settings as config
 from routes import setup_routes
 
-logging.basicConfig(level=config.LEVEL, filename='log/main.log')
 
-
-def main():
-    loop = asyncio.get_event_loop()
-    app = web.Application(loop=loop)
-    setup_routes(app)
-    setup(app, EncryptedCookieStorage(config.SECRET_KEY))
-    aiohttp_jinja2.setup(app,
-                         loader=jinja2.FileSystemLoader(config.TEMPLATES_ROOT))
-    app.router.add_static('/static', config.STATIC_ROOT, show_index=True)
-
-    web.run_app(app, host=config.HOST, port=config.PORT)
-
-if __name__ == '__main__':
-    main()
-
-
+app = web.Application()
+setup_routes(app)
+setup(app, EncryptedCookieStorage(config.SECRET_KEY))
+aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader(config.TEMPLATES_ROOT))
+app.router.add_static('/static', config.STATIC_ROOT, show_index=True)
