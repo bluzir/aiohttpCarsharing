8b6e082b30db6b2c170563c7c8bbe5661e94164f
Vladislav <kouklevv@gmail.com>
2017-02-27 20:41:25 +0300

    Starline test methods

--- lib/external_api/starline_wrapper.py
+++ lib/external_api/starline_wrapper.py
@@ -5,21 +5,29 @@ class StarLineClient(BaseClient):
     DEFAULT_HOST = 'starline.ru'
     DEV_HOST = 'https://dev-id.{}/:4444'.format(DEFAULT_HOST)
 
-    def __init__(self, data, cookie):
-        self.data = data
-        self.cookies = None
+    def __init__(self):
+        self.slid = None
+        self.url = self.DEV_HOST
+        super(StarLineClient, self).__init__()
 
-    def device_sync(self, id, type, command):  # Temporary naming
-        data =  {
-            'id': id,
-            'type': type,
-            command['name']: command['value'],
+    async def device_sync(self, command, value):  # Temporary naming
+        self.data = {
+            'id': self.slid,
+            'type': command,
+            command: value,
         }
-        response = self.post(url=self.DEV_HOST,
-                             data=data,
-                             cookies=self.cookies)
-
+        response = await self.post()
         return response
+
+    async def ign_start(self):  # Method that starts the car engine
+        response = await self.device_sync(command="ign_start", value=1)
+        return response
+
+    async def ign_stop(self):  # Method that stops the car engine
+        response = await self.device_sync(command="ign_stop", value=1)
+        return response
+
+    
 
 
 
