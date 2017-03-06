91e7261917b5c2c7254a9b3abf5a62c05f63358e
Andrei Bodrov <ap.bodrov@gmail.com>
2017-03-06 22:55:52 +0300

    окончательный фикс

--- controller.py
+++ controller.py
@@ -259,9 +259,9 @@ async def api_inplat_callback(request):
     query = request.rel_url.query
     logging.debug(query)
 
-    data = await request.post()
+    data = await request.json()
     logging.debug(data)
-    logging.debug(request.POST)
+
 
     return Response()
 
